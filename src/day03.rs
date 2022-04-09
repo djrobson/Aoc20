9df7f6914ca7d84165506a3b7ccbef0f78f8bd04
Dale Robson <drobson@gmail.com>
2022-04-09 19:23:35 -0400

    day3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -15,5 +15,30 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
+    // .#..#....##...#....#.....#.#...
+    let nums: Vec<&[u8]> = input.lines()
+        .map(|x| x.as_bytes())
+        .collect();
+    let r_len = nums[0].len();
+    let mut counts: Vec<usize> = Vec::new();
+    let slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)];
+    for (c_stride, r_stride) in slopes {
+        let mut count = 0;
+        // walk down by row stride
+        let mut col = 0;
+        let mut row = 0;
+
+        while row < nums.len(){
+            
+            if nums[row][col%r_len] == '#' as u8 {
+                count = count + 1;
+            }
+            col = col + c_stride;
+            row = row + r_stride;
+        }
+        println!("{}", count);
+        counts.push(count);
+    }
+    println!("{}", counts.iter().fold(1, |acc,&x| acc*x));
 
 }
