88225c534a18bc7c6a60e40915e3967bb1341007
Dale Robson <drobson@gmail.com>
2022-04-09 18:39:46 -0400

    day02, the server had given us errors in the input?!

--- src/day02.rs
+++ src/day02.rs
@@ -1,5 +1,24 @@
 pub fn part1(input: String) {
+    // 2-4 d: mvdclzddj
+    let raw_lines: Vec<&str> = input.lines().collect();
+    let mut total = 0;
+    for line in raw_lines {
+        let mut split = line.split(' ');
+        let (r, l, s) = (split.next().unwrap(), 
+                                            split.next().unwrap().chars().nth(0).unwrap(),
+                                            split.next().unwrap_or_else(|| panic!("{}", line)));
+        let mut rsplit = r.split('-');
+        let r1 = rsplit.next().unwrap().parse::<usize>().unwrap();
+        let r2 = rsplit.next().unwrap().parse::<usize>().unwrap();
+        let c = s.matches(l).count();
+        if c >= r1 && c <= r2 {
+            total = total +1;
+        } else {
+            total = total +0;
+        }
 
+    }
+    println!("{}", total);
 }
 
 pub fn part2(input: String) {
