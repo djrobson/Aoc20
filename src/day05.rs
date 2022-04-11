ffcdb4ef31f095cc4d74bede2f4e75568853bfdd
Dale Robson <drobson@gmail.com>
2022-04-11 14:44:53 -0400

    day05 part2 done

--- src/day05.rs
+++ src/day05.rs
@@ -18,5 +18,24 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
-  ()
+    let mut seats: [u8;1024] = [0;1024];
+
+    for l in input.lines() {
+        let b = l.as_bytes();
+        let mut this: usize = 0;
+        let len = 9;
+        for o in 0..=len {
+            let my_o = len -o;
+            if b[my_o] == 'B' as u8 || b[my_o] == 'R' as u8 {
+                this = this + (1<<o);
+            }
+        }
+        seats[this] = 1;
+    }
+    for s in 0..seats.len() {
+        if seats[s] == 0 {
+            println!("{}", s);
+        }
+    }
+
 }
