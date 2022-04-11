3a36d069978ac87ea500f4534ec439cb8ca493c0
Dale Robson <drobson@gmail.com>
2022-04-11 12:44:18 -0400

    start of day4, input was line merged

---
+++ src/day04.rs
@@ -0,0 +1,12 @@
+pub fn part1(input: String) {
+    // .#..#....##...#....#.....#.#...
+    let nums: Vec<&[u8]> = input.lines()
+        .map(|x| x.as_bytes())
+        .collect();
+}
+
+pub fn part2(input: String) {
+    // .#..#....##...#....#.....#.#...
+    ()
+
+}
